ff8216668840166c4c6e239dac33863a857c6bc0
bc-dipesh <D.Bc8829@student.leedsbeckett.ac.uk>
2020-02-07 14:07:38 +0545

    Updates

---
+++ precache-manifest.d6fd90c5894159831b35bbfd8c1da388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62593a3ad3b6bb87dfd35f48f68a5393",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ab88e012ded3738e2476",
+    "url": "/robofriends/static/css/main.cdc310f1.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab88e012ded3738e2476",
+    "url": "/robofriends/static/js/main.842eece4.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
