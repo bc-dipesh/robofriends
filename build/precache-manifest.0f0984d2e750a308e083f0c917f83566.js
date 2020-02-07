c89e400afa40394313a3181b4d0e8e556b262daa
bc-dipesh <50456672+bc-dipesh@users.noreply.github.com>
2020-02-07 13:49:09 +0545

    Add files via upload

---
+++ build/precache-manifest.0f0984d2e750a308e083f0c917f83566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15529fa4b62673e248cf2ae0800bc73",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "67b82db31035e33c68cb",
+    "url": "/robofriends/static/css/main.cdc310f1.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b82db31035e33c68cb",
+    "url": "/robofriends/static/js/main.ab55567b.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
